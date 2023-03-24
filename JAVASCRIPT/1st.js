c6b0a3e6f1ccc7aa686a3d97278f3bffb8d56deb
Karanpreet Singh <karanpreetsingh817@gmail.com>
2023-03-24 15:14:06 +0530

    First comment

---
+++ JAVASCRIPT/1st.js
@@ -0,0 +1,51 @@
+// x="Karanpreet Singh";
+// y="9"
+
+// console.log(`my name is ${x} and my date of month is ${y}`);
+// const k={
+//     name:"karan",
+//     lastname:"singh",
+//     rollno:1916081,
+//     print:function(){
+//         console.log(`my full name is ${this.name} ${this.lastname} and my roll numer is ${this.rollno}`)
+//     }
+// }
+
+// k.print();
+// create a variale of type string and try to add numer to it
+// let x="1515";
+// console.log(typeof(x+1000000));
+// const oj={
+//     name:"karan",
+//     age:22,  
+
+// }
+
+// oj.roll=34;
+// console.log(oj)
+// const dic={
+//     consider:"deem to be",
+//     minute:"infinitely or immeasurably small",
+//     accord:"concurrence of opinion",
+//     evident:"clearly revealed to the mind or the senses or judgment",
+//     practice:"a customary way of operation or behavior",
+//     intend:"have in mind as a purpose",
+//     concern:"something that interests you because it is important",
+//     commit:"committed; committing; commits",
+//     issue:"some situation or event that is thought about",
+//     approach:"move towards"
+// }
+
+// dic.establish="set up or found";
+// dic.utter="without qualification";
+// dic.conduct="direct the cource of;manage or control";
+// dic.engage="consume all of one's time";
+// dic.obtain="come into possession of";
+// console.log(dic )
+
+// let a=28;
+// a++;
+// console.log(a)
+
+
+
